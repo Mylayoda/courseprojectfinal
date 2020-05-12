3fabbedcb11c58c0a52bde9219fc5a91a8221aff
Derrick Yirenkyi <21703340@bucks.ac.uk>
2020-05-12 18:08:39 +0100

    new git sourse and migration

---
+++ courses/Migrations/202005121708006_CourseModule1.cs
@@ -0,0 +1,16 @@
+namespace courses.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CourseModule1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
